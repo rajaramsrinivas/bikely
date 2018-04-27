e30ab8ab93793dc886ddc6a799ed0b072daba7dc
rajaramsrinivas <rajaram.1990@gmail.com>
2018-04-27 14:56:55 -0400

    data csv: write to Mongo stations and trips

--- server/app.js
+++ server/app.js
@@ -1,7 +1,32 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+async function writeDocToDb(docToWrite) {
+  let client;
+
+  try {
+    client = await MongoClient.connect(url);
+    console.log("Connected correctly to server");
+
+    const db = client.db(dbName);
+
+    // Insert multiple documents
+    r = await db.collection('inserts').insertMany(docToWrite);
+    client.close()
+    return r.insertedCount;
+  } catch (err) {
+    console.log(err.stack);
+  }
+
+  // Close connection
+  client.close();
+};
+
+
+app.get('/', (req, res) => res.send('Hello World!'+req.query.q))
 app.get('/test', (req, res) => res.send('Hello World2!'))
 
-app.listen(80, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
