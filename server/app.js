d685e2fc66ea3c75305101b1ccd3b0fef728cb44
rajaramsrinivas <rajaram.1990@gmail.com>
2018-05-03 01:51:54 -0400

    App.js that server html with fetch

--- server/app.js
+++ server/app.js
@@ -1,32 +1,54 @@
 const express = require('express')
 const app = express()
-
+const dbName = "bikely"; 
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const url = 'mongodb://localhost:27017';  
+var clientObj = null;
+async function connectMongo() {
+    try {
+        const client = await MongoClient.connect(url);
+        console.log("Connected successfully");
+        return client;
+    } 
+    catch (err) {
+        console.log(err.error.stack);
+    }
+}
 
-async function writeDocToDb(docToWrite) {
-  let client;
+(async function(){
+    try {
+        clientObj = await connectMongo();
+        console.log('Connected!');
+    }
+    catch (err) {
+        console.log(err.error.stack);
+    }
+});
 
-  try {
-    client = await MongoClient.connect(url);
-    console.log("Connected correctly to server");
-
-    const db = client.db(dbName);
-
-    // Insert multiple documents
-    r = await db.collection('inserts').insertMany(docToWrite);
-    client.close()
-    return r.insertedCount;
-  } catch (err) {
-    console.log(err.stack);
-  }
-
-  // Close connection
-  client.close();
-};
-
-
-app.get('/', (req, res) => res.send('Hello World!'+req.query.q))
+var readDocs = null;
+var db = null;
+app.get('/', (req, res) => {
+            //const docs = await clientObj.db(dbName).collection('trips').find({}).toArray();
+            //res.sendFile(__dirname + '/../client/index.html');
+            db.collection('stations').find().toArray(function(err,results){
+                res.send(results); 
+                //res.sendFile(__dirname + '/index.html');
+            })
+        });
+app.get('/stations', (req,res) => {
+    console.log(__dirname);
+    res.sendFile(__dirname+"/public/stations2.html");
+});
+app.get('/frequency', (req,res) => {
+    console.log(__dirname);
+    res.sendFile(__dirname+"/public/mapbox.html");
+})
 app.get('/test', (req, res) => res.send('Hello World2!'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+MongoClient.connect(url, (err, client) => {
+    if (err) return console.log(err)
+    db = client.db(dbName); // whatever your database name is
+    app.listen(3000, () => {
+      console.log('listening on 3000');
+    })
+  })
